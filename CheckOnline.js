e360bdb247a00b30a7e9c81e4e6ed3d46f267018
Euan <masonator117@gmail.com>
2018-12-17 18:14:29 +0000

    trying something with livestreams

--- CheckOnline.js
+++ CheckOnline.js
@@ -93,6 +93,18 @@ $(function() {
 					}
 
           console.log(liveStreamers);
+          for (var i = 0; i < liveStreamers.length; i++) {
+            var source = "https://player.twitch.tv/?"+liveStreamers[i];
+            <iframe
+              src = source;
+              height="<height>"
+              width="<width>"
+              frameborder="<frameborder>"
+              scrolling="<scrolling>"
+              allowfullscreen="<allowfullscreen>">
+            </iframe>
+
+          }
 				}
 			}
 		});
