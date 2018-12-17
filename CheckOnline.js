87cc8b787ea1861060888025c9a4401fe2efe802
Euan <masonator117@gmail.com>
2018-12-17 17:19:59 +0000

    Testing out codepen stuff

---
+++ CheckOnline.js
@@ -0,0 +1,95 @@
+$(function() {
+	//establish variables
+	var $displayTable = $("#displayTable");
+	var $displayTable2 = $("#displayTable2");
+	var loginNames = ["ESL_SC2", "cretetion", "freecodecamp", "cdnthe3rd", "drdisrespectlive", "jukes", "xqcow", "noobs2ninjas"]
+	var urlString = "";
+	var urlString2 = "";
+	var statusString = "";
+	var htmlString = "";
+	var htmlString2 = "";
+
+//create urlString with loginName array
+	for (var i = 0; i < loginNames.length; i++){
+		if (loginNames.length > i-1) {
+			urlString += "login=" + loginNames[i] + "&";
+			statusString += "user_login=" + loginNames[i] + "&";
+		} else {
+			urlString += "login=" + loginNames[i];
+			statusString += "user_login=" + loginNames[i];
+		}
+
+//get user data
+	}
+	$.ajax({
+		type: 'GET',
+		url: "https://api.twitch.tv/helix/users?" + urlString,
+		headers: {
+			'Client-ID': 'tmqo5l8t8uylxhoct7vzqrzjr22zce'
+		},
+		success: function(profileData) {
+			console.log(profileData);
+
+			createTable(profileData);
+		}
+	});
+
+//display userdata
+	function createTable (profileData) {
+
+		for (var j = 0; j < profileData.data.length; j++){
+			htmlString +=
+			"<div class=\"row\"><div class=\"col span-1-of-12\"><img src=\"" +
+			profileData.data[j].profile_image_url +
+			"\" height=\"70px\" width=\"70px\"></div><div class=\"col span-4-of-12\"><h2><a href=\"https://www.twitch.tv/" +
+			profileData.data[j].display_name +
+			"\" target=\"_blank\">" +
+			profileData.data[j].display_name +
+			"</a></h2><h5 id=\"" +
+			profileData.data[j].display_name +
+			"\">Offline</h5></div><div class=\"col span-7-of-12\"><h4 id=\"desc" +
+			profileData.data[j].display_name +
+			"\">" +
+			profileData.data[j].description +
+			"</h4></div></div> ";
+
+		}
+
+//console.log(htmlString);
+		displayTable.insertAdjacentHTML("beforeend", htmlString);
+		checkStatus(profileData);
+	};
+
+//check to see if users are offline or live
+	function checkStatus(profileData) {
+		$.ajax({
+			type: 'GET',
+			url: "https://api.twitch.tv/helix/streams?" + statusString,
+			headers: {
+				'Client-ID': 'tmqo5l8t8uylxhoct7vzqrzjr22zce'
+			},
+			success: function(newData) {
+
+			if (newData.data.length === 0) {
+
+
+
+				} else {
+					for (var i=0; i<newData.data.length; i++){
+						for (var j = 0; j<profileData.data.length; j++){
+							console.log(newData.data[i].user_id);
+							console.log(profileData.data[j].id);
+							if(newData.data[i].user_id === profileData.data[j].id) {
+								var profileStatus = document.getElementById(profileData.data[j].display_name);
+								var descName = "desc"+ profileData.data[j].display_name;
+								var newDescription = document.getElementById(descName);
+								profileStatus.innerHTML = "<strong class=\"live\">Live</strong>";
+								newDescription.innerHTML = newData.data[i].title;
+							}
+						}
+					}
+				}
+			}
+		});
+	};
+});
