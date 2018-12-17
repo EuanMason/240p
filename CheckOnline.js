c2efada8b592240a23c6dd963c28461ae24329e3
Euan <masonator117@gmail.com>
2018-12-17 18:54:49 +0000

    test again

--- CheckOnline.js
+++ CheckOnline.js
@@ -95,14 +95,15 @@ $(function() {
           console.log(liveStreamers);
           for (var i = 0; i < liveStreamers.length; i++) {
             var source = "https://player.twitch.tv/?"+liveStreamers[i];
-            // <iframe
-            //   src = source;
-            //   height="<height>"
-            //   width="<width>"
-            //   frameborder="<frameborder>"
-            //   scrolling="<scrolling>"
-            //   allowfullscreen="<allowfullscreen>">
-            // </iframe>
+
+            <iframe
+                src=source
+                height="720"
+                width="1280"
+                frameborder="0"
+                scrolling="no"
+                allowfullscreen="true">
+            </iframe>
 
           }
 				}
